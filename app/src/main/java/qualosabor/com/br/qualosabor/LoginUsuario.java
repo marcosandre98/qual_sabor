9888956c8c95d21362c8edb29a57faf415db7d6c
marcosandre98 <36673475+marcosandre98@users.noreply.github.com>
2018-10-22 11:41:44 -0200

    Implementado a leitura do QR Code

--- app/src/main/java/qualosabor/com/br/qualosabor/LoginUsuario.java
+++ app/src/main/java/qualosabor/com/br/qualosabor/LoginUsuario.java
@@ -1,11 +1,18 @@
 package qualosabor.com.br.qualosabor;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class LoginUsuario extends AppCompatActivity {
 
@@ -22,5 +29,33 @@ public class LoginUsuario extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Login Usuário");
 
+        final Activity activity = this;
+
+        imgBtnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(activity);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                integrator.setPrompt("Scan");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(false);
+                integrator.setBarcodeImageEnabled(false);
+                integrator.initiateScan();
+            }
+        });
+        }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null){
+            if(result.getContents()== null){
+                Toast.makeText(this, "Você cancelou a leitura!", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }

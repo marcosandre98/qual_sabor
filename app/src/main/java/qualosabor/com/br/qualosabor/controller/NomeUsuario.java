6c44a234a124901e1f9721ec12870cca91a1c438
eduardovilke <36681539+eduardovilke@users.noreply.github.com>
2018-11-28 21:38:51 -0200

    Alterções login facebook

--- app/src/main/java/qualosabor/com/br/qualosabor/controller/NomeUsuario.java
+++ app/src/main/java/qualosabor/com/br/qualosabor/controller/NomeUsuario.java
@@ -1,13 +1,29 @@
 package qualosabor.com.br.qualosabor.controller;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.util.Arrays;
 
 import qualosabor.com.br.qualosabor.R;
 import qualosabor.com.br.qualosabor.dao.CadastroClienteDAO;
@@ -15,10 +31,18 @@ import qualosabor.com.br.qualosabor.dao.CadastroSaborDAO;
 import qualosabor.com.br.qualosabor.models.Cliente;
 import qualosabor.com.br.qualosabor.models.Sabor;
 
+
 public class NomeUsuario extends AppCompatActivity {
 
+    CallbackManager callbackManager;
     private EditText txtNomeCliente;
+    private Button login_button;
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +55,30 @@ public class NomeUsuario extends AppCompatActivity {
         String mesa = it.getStringExtra("NumeroMesa");
         TextView label = findViewById(R.id.txtNomeUsu);
         label.setText("Código scaneado com sucesso na Mesa "+mesa+"! Por favor, insira seu nome:");
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginButton loginButton = (LoginButton)findViewById(R.id.login_button);
+        //loginButton.setFragment(this);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+            
+        });
     }
 
     public void cadastroCliente (View view){ // View V serve para ação de CLICK

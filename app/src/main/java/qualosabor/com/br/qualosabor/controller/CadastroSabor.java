d6449182fba1fdd82274f00efac90b2a28ff2928
lucasG3Sistemas <lucas@g3sistemas.com>
2018-12-04 20:35:19 -0200

    adicionar imagem da pizza

--- app/src/main/java/qualosabor/com/br/qualosabor/controller/CadastroSabor.java
+++ app/src/main/java/qualosabor/com/br/qualosabor/controller/CadastroSabor.java
@@ -1,6 +1,15 @@
 package qualosabor.com.br.qualosabor.controller;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +17,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import qualosabor.com.br.qualosabor.R;
@@ -20,6 +30,9 @@ public class CadastroSabor extends AppCompatActivity {
 
     private EditText txtNomeSabor;
     private EditText txtIngre;
+    private ImageView imagem;
+    private final int GALERIA_IMAGENS = 1;
+    private  final int PERMISSAO_REQUEST = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +42,55 @@ public class CadastroSabor extends AppCompatActivity {
         txtNomeSabor = (EditText) CadastroSabor.this.findViewById(R.id.txtNomeSabor);
         txtIngre = (EditText) CadastroSabor.this.findViewById(R.id.txtIngre);
 
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSAO_REQUEST);
+            }
+        }
+
+        imagem = (ImageView) findViewById(R.id.ivImagemSabor);
+        Button galeria = (Button) findViewById(R.id.btnGaleria);
+        galeria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, GALERIA_IMAGENS);
+            }
+        });
+
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Cadastrar Sabor");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == GALERIA_IMAGENS) {
+            Uri selectedImage = data.getData();
+            String [] filePath = {MediaStore.Images.Media.DATA};
+            Cursor c = getContentResolver().query(selectedImage, filePath, null, null, null);
+            c.moveToFirst();
+            int columnIndex = c.getColumnIndex(filePath[0]);
+            String picturePath = c.getString(columnIndex);
+            c.close();
+            Bitmap imagemGaleria = (BitmapFactory.decodeFile(picturePath));
+            imagem.setImageBitmap(imagemGaleria);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (requestCode == PERMISSAO_REQUEST) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            } else {
+
+            }
+            return;
+        }
     }
 
     public void cadastrarSabor(View view){ // View V serve para ação de CLICK
--- app/src/main/java/qualosabor/com/br/qualosabor/controller/ListarSabores.java
+++ app/src/main/java/qualosabor/com/br/qualosabor/controller/ListarSabores.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
